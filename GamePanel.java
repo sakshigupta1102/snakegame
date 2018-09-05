aa4760b76d21e696694bc72606fa433682903ce3
sakshigupta1102 <36024324+sakshigupta1102@users.noreply.github.com>
2018-09-05 11:44:09 +0530

    Add files via upload

---
+++ GamePanel.java
@@ -0,0 +1,287 @@
+package game;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable, KeyListener {
+
+	
+	public static final int WIDTH=600;
+	public static final int HEIGHT=600;
+	
+	
+	//render
+	private Graphics2D g2d;
+	private BufferedImage image;
+	
+	//game loop 
+	private Thread thread;
+	private boolean running;
+	private long targetTime;
+	
+	Entity head,apple;
+	ArrayList<Entity> snake;
+	int score=0;
+	private final int SIZE=10;
+	
+    //movement
+	private int dx,dy;
+	//key input
+	
+	private boolean up,down,right,left,start;
+	
+	
+	
+	
+	
+	
+	
+	public GamePanel(){
+		
+		setPreferredSize(new Dimension(WIDTH,HEIGHT));
+		setFocusable(true);
+		requestFocus();
+		addKeyListener(this);
+		
+		
+		
+	}
+	public void addNotify(){
+		super.addNotify();
+		thread=new Thread(this);
+		thread.start();
+	}
+	
+	
+	private void setFPS(int fps)
+	{
+		targetTime=1000/fps;
+		
+	}
+	
+	
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		int k=e.getKeyCode();
+		if(k== KeyEvent.VK_UP) up=true;
+		if(k==KeyEvent.VK_DOWN) down=true;
+		if(k==KeyEvent.VK_LEFT) left=true;
+		if(k==KeyEvent.VK_RIGHT) right=true;
+		if(k==KeyEvent.VK_ENTER) start=true;
+		
+		
+
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+	
+		int k=e.getKeyCode();
+		if(k== KeyEvent.VK_UP) up=false;
+		if(k==KeyEvent.VK_DOWN) down=false;
+		if(k==KeyEvent.VK_LEFT) left=false;
+		if(k==KeyEvent.VK_RIGHT) right=false;
+		if(k==KeyEvent.VK_ENTER) start=false;
+		
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+
+	}
+
+	@Override
+	public void run() {
+	 if(running) return;
+	 init();
+	 long elapsed = 0;
+	 long wait;
+	 while(running){
+		 System.nanoTime();
+		 update();
+		 requestRender();
+		 wait =targetTime- elapsed/1000000;
+		 if(wait>0){
+			 try{
+				 Thread.sleep(wait);
+				 
+			 }catch(Exception e)
+			 {
+				 e.printStackTrace();
+			 }
+		 }
+		 
+	 }
+          
+	}
+	
+	private void setUplevel(){
+		snake=new ArrayList<Entity>();
+		head=new Entity(SIZE);
+		head.setPosition(WIDTH/2, HEIGHT/2);
+		snake.add(head);
+		
+		for(int i = 0;i<3;i++)
+		{
+			Entity e=new Entity(SIZE);
+			e.setPosition(head.getX()+(i*SIZE), head.getY());
+			snake.add(e);
+			
+			
+		}
+		
+	
+	
+apple= new Entity(SIZE);
+setapple();
+int score;
+
+	}
+	
+	public void setapple(){
+	int x=(int) (Math.random()*(WIDTH-SIZE));
+	
+	int y=(int) (Math.random()*(HEIGHT-SIZE));
+	apple.setPosition(x, y);
+	x=x-(x% SIZE);
+	
+	y=y-(y% SIZE);
+	
+	
+	}
+	
+	
+	
+	private void requestRender() {
+		render(g2d);
+		Graphics g= getGraphics();
+		g.drawImage(image, 0, 0, null);
+		g.dispose();
+		
+		
+	}
+	
+
+	private void update() {
+		if(up && dy==0)
+		{
+			dy=-SIZE;
+			dx=0;
+		}
+		
+		if(down && dy==0)
+		{
+			dy=SIZE;
+			dx=0;
+		}
+		if(left && dx==0)
+		{
+			dy=0;
+			dx=SIZE;
+		}
+		if(right && dx==0)
+		{
+			dy=0;
+			dx=SIZE;
+		}
+		
+		
+		if(dx!=0||dy!=0)
+		{
+			
+		for(int i=snake.size()-1;i>0;i--){
+			snake.get(i).setPosition(
+					snake.get(i-1).getX(), snake.get(i-1).getY());
+		}
+		head.move(dx, dy);
+		
+		}
+		if(apple.isCollision(head))
+		{
+			score++;
+			setapple();
+			Entity e=new Entity(SIZE);
+			e.setPosition(-100,-100);
+			snake.add(e);
+			
+			
+			
+		}
+		
+		if(head.getX() <0)
+			head.setX(WIDTH);
+		if(head.getY() <0)
+			head.setY(HEIGHT);
+		if(head.getX()  > WIDTH)
+			head.setX(0);
+		if(head.getY()  >HEIGHT ) 
+			head.setY(0);
+		
+		
+		
+		
+		
+		
+			
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	private void render(Graphics2D g2d) {
+		// TODO Auto-generated method stub
+		g2d.clearRect(20, 20, WIDTH,HEIGHT);
+		g2d.setColor(Color.GREEN);
+		for(Entity e:snake)
+		{
+			e.render(g2d);
+			
+			
+			}
+		g2d.setColor(Color.RED);
+		apple.render(g2d);
+		g2d.setColor(Color.RED);
+		g2d.drawString("score  " + score, 10, 10);
+		}
+	
+	
+	
+	
+	
+	private void init()
+	{
+		image= new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_ARGB);
+		g2d=image.createGraphics();
+		running=true;
+		setFPS(10);
+		setUplevel();
+		
+	}
+	
+	
+	
+	
+
+}
